cc673b6ae5bbbd6719305736e8f8f714d654ba3c
mac <715134026@qq.com>
2016-09-07 20:04:19 +0800

    如图所示

---
+++ ZYDraggableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYDraggableViewDemo
+//
+//  Created by  on 16/8/26.
+//  Copyright © 2016年 tongbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYDraggableViewDemo/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  ZYDraggableViewDemo
+//
+//  Created by  on 16/8/26.
+//  Copyright © 2016年 tongbu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIView+ZYDraggable.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // UIImageView ignored user events by default, so set
+    // `userInteractionEnabled` to YES for receive touch events.
+    self.avatarView.userInteractionEnabled = YES;
+    
+    // Make avatarView draggable
+    [self.avatarView makeDraggable];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    // Update snap point when layout occured
+    [self.avatarView updateSnapPoint];
+}
+
+@end
